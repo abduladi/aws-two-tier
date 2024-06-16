0ab5e2b5db0613ac61449b02b9b6f9dee6973ae4
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-16 20:35:33 +0200

    updated s3

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,19 +1,19 @@
 
 
-# Configure CloudTrail to log s3 events
-resource "aws_cloudtrail" "s3_event_log" {
-  name                          = "my-cloudtrail"
-  s3_bucket_name                = aws_s3_bucket.cloudtrail_log_bucket.id
-  include_global_service_events = true
-  is_multi_region_trail         = true
+# # Configure CloudTrail to log s3 events
+# resource "aws_cloudtrail" "s3_event_log" {
+#   name                          = "my-cloudtrail"
+#   s3_bucket_name                = aws_s3_bucket.cloudtrail_log_bucket.id
+#   include_global_service_events = true
+#   is_multi_region_trail         = true
 
-  event_selector {
-    read_write_type           = "All"
-    include_management_events = true
+#   event_selector {
+#     read_write_type           = "All"
+#     include_management_events = true
 
-    data_resource {
-      type   = "AWS::S3::Object"
-      values = [aws_s3_bucket.data_bucket.arn]
-    }
-  }
-}
+#     data_resource {
+#       type   = "AWS::S3::Object"
+#       values = [aws_s3_bucket.data_bucket.arn]
+#     }
+#   }
+# }
