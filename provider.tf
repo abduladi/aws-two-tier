0f6d3594e0a1b7ff8f2f61b1923fffeb5952e960
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-16 04:49:38 +0200

    updated provider for HCP integration

--- provider.tf
+++ provider.tf
@@ -7,9 +7,27 @@ terraform {
       version = ">=5.0.0"
     }
   }
+
+
+# Integration with HCP
+  cloud {
+    organization = "N26-demo"
+
+    workspaces {
+      name = "n26-demo"
+    }
+  }
+
+
 }
 
 provider "aws" {
   region = "us-east-1"
   # profile = "terraform"
+}
+
+
+
+terraform {
+  
 }
