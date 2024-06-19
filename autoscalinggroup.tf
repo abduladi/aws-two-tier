9e2c44f8e8204b923a1b343890d44aef9fe36c61
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-19 06:09:46 +0200

    updated autoscaling with launch template

--- autoscalinggroup.tf
+++ autoscalinggroup.tf
@@ -49,7 +49,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
   }
 
   filter {
