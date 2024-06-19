3aca855265de9715ba44bfef09acb5b93b0591fd
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-19 05:21:42 +0200

    updated autoscaling with launch template

--- autoscalinggroup.tf
+++ autoscalinggroup.tf
@@ -81,30 +81,50 @@ resource "aws_launch_configuration" "n26_launch_config" {
 
 
 # ASG
+# resource "aws_autoscaling_group" "n26_asg" {
+#   name                 = "n26_asg"
+#   desired_capacity     = 2
+#   max_size             = 4
+#   min_size             = 2
+#   force_delete         = true
+#   depends_on           = [aws_lb.n26_alb]
+#   target_group_arns    = ["${aws_lb_target_group.n26_alb_target_group.arn}"]
+#   health_check_type    = "ELB"
+#   vpc_zone_identifier  = ["${aws_subnet.private-subnet-1.id}", "${aws_subnet.private-subnet-2.id}"]
+#   wait_for_capacity_timeout = "20m"
+
+
+#   launch_template {
+#     id      = aws_launch_template.n26_launch_template.id
+#     version = aws_launch_template.n26_launch_template.latest_version
+#   }
+
+#   tag {
+#     key                 = "Name"
+#     value               = "n26_asg"
+#     propagate_at_launch = true
+#   }
+
+# }
+
+
 resource "aws_autoscaling_group" "n26_asg" {
   name                 = "n26_asg"
   desired_capacity     = 2
   max_size             = 4
-  min_size             = 2
+  min_size             = 1
   force_delete         = true
   depends_on           = [aws_lb.n26_alb]
   target_group_arns    = ["${aws_lb_target_group.n26_alb_target_group.arn}"]
-  health_check_type    = "ELB"
+  health_check_type    = "EC2"
+  launch_configuration = aws_launch_configuration.n26_launch_config.name
   vpc_zone_identifier  = ["${aws_subnet.private-subnet-1.id}", "${aws_subnet.private-subnet-2.id}"]
-  wait_for_capacity_timeout = "20m"
-
-
-  launch_template {
-    id      = aws_launch_template.n26_launch_template.id
-    version = aws_launch_template.n26_launch_template.latest_version
-  }
 
   tag {
     key                 = "Name"
     value               = "n26_asg"
     propagate_at_launch = true
   }
-
 }
 
 # Target group
