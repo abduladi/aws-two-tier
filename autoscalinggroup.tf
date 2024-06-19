1df3e2f755da412c6282b30359100d6d200737ae
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-19 13:13:26 +0200

    updated autoscaling with launch template

--- autoscalinggroup.tf
+++ autoscalinggroup.tf
@@ -1,46 +1,26 @@
-# data "aws_ami" "amazon-linux-2" {
-#   most_recent = true
+resource "aws_launch_template" "n26_launch_template" {
+  name_prefix   = "n26-launch-template"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-#   filter {
-#     name   = "name"
-#     values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-#   }
+  vpc_security_group_ids = [aws_security_group.webserver-security-group.id]
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
-
-# }
-
-# resource "aws_launch_template" "n26_launch_template" {
-#   name_prefix   = "n26-launch-template"
-#   image_id      = data.aws_ami.amazon-linux-2.id
-#   instance_type = "t2.micro"
-
-#   vpc_security_group_ids = [aws_security_group.webserver-security-group.id]
-
-#   block_device_mappings {
-#     device_name = "/dev/sda1"
+  block_device_mappings {
+    device_name = "/dev/sda1"
     
-#     ebs {
-#       volume_type = "gp3"
-#       volume_size = 10
-#       encrypted   = true
-#     }
-#   }
+    ebs {
+      volume_type = "gp3"
+      volume_size = 10
+      encrypted   = true
+    }
+  }
 
-#   user_data = filebase64("${path.module}/bootstrap_webserver.sh")
+  user_data = filebase64("${path.module}/bootstrap_webserver.sh")
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 
 
@@ -60,72 +40,73 @@ data "aws_ami" "ubuntu" {
 }
 
 
-resource "aws_launch_configuration" "n26_launch_config" {
-  name_prefix     = "n26-launch-config"
-  image_id        = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  security_groups = ["${aws_security_group.webserver-security-group.id}"]
+# resource "aws_launch_configuration" "n26_launch_config" {
+#   name_prefix     = "n26-launch-config"
+#   image_id        = data.aws_ami.ubuntu.id
+#   instance_type   = "t2.micro"
+#   security_groups = ["${aws_security_group.webserver-security-group.id}"]
 
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 10
-    encrypted   = true
-  }
+#   root_block_device {
+#     volume_type = "gp3"
+#     volume_size = 10
+#     encrypted   = true
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-  user_data = filebase64("${path.module}/bootstrap_webserver.sh")
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+#   user_data = filebase64("${path.module}/bootstrap_webserver.sh")
+# }
 
 
 
 # ASG
-# resource "aws_autoscaling_group" "n26_asg" {
-#   name                 = "n26_asg"
-#   desired_capacity     = 2
-#   max_size             = 4
-#   min_size             = 2
-#   force_delete         = true
-#   depends_on           = [aws_lb.n26_alb]
-#   target_group_arns    = ["${aws_lb_target_group.n26_alb_target_group.arn}"]
-#   health_check_type    = "ELB"
-#   vpc_zone_identifier  = ["${aws_subnet.private-subnet-1.id}", "${aws_subnet.private-subnet-2.id}"]
-#   wait_for_capacity_timeout = "20m"
-
-
-#   launch_template {
-#     id      = aws_launch_template.n26_launch_template.id
-#     version = aws_launch_template.n26_launch_template.latest_version
-#   }
-
-#   tag {
-#     key                 = "Name"
-#     value               = "n26_asg"
-#     propagate_at_launch = true
-#   }
-
-# }
-
-
 resource "aws_autoscaling_group" "n26_asg" {
   name                 = "n26_asg"
   desired_capacity     = 2
   max_size             = 4
-  min_size             = 1
+  min_size             = 2
   force_delete         = true
   depends_on           = [aws_lb.n26_alb]
   target_group_arns    = ["${aws_lb_target_group.n26_alb_target_group.arn}"]
-  health_check_type    = "EC2"
-  launch_configuration = aws_launch_configuration.n26_launch_config.name
+  health_check_type    = "ELB"
   vpc_zone_identifier  = ["${aws_subnet.private-subnet-1.id}", "${aws_subnet.private-subnet-2.id}"]
+  wait_for_capacity_timeout = "20m"
+
+
+  launch_template {
+    id      = aws_launch_template.n26_launch_template.id
+    version = aws_launch_template.n26_launch_template.latest_version
+  }
 
   tag {
     key                 = "Name"
     value               = "n26_asg"
     propagate_at_launch = true
   }
+
 }
+
+
+# ASG using launch configuration
+# resource "aws_autoscaling_group" "n26_asg" {
+#   name                 = "n26_asg"
+#   desired_capacity     = 2
+#   max_size             = 4
+#   min_size             = 1
+#   force_delete         = true
+#   depends_on           = [aws_lb.n26_alb]
+#   target_group_arns    = ["${aws_lb_target_group.n26_alb_target_group.arn}"]
+#   health_check_type    = "EC2"
+#   launch_configuration = aws_launch_configuration.n26_launch_config.name
+#   vpc_zone_identifier  = ["${aws_subnet.private-subnet-1.id}", "${aws_subnet.private-subnet-2.id}"]
+
+#   tag {
+#     key                 = "Name"
+#     value               = "n26_asg"
+#     propagate_at_launch = true
+#   }
+# }
 
 # Target group
 resource "aws_lb_target_group" "n26_alb_target_group" {
