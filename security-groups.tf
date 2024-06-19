e1d6b1818c4f552aa67859e20c6a161de32c3fc5
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-19 04:14:56 +0200

    updated autoscaling with launch template

--- security-groups.tf
+++ security-groups.tf
@@ -39,6 +39,14 @@ resource "aws_security_group" "webserver-security-group" {
     security_groups  = [aws_security_group.alb-security-group.id]
   }
 
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.alb-security-group.id]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
