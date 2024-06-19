1a0e4b20c3b602453f791519a78105b35a679ff0
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-20 00:15:05 +0200

    updated ssl cert to be retrieved from ACM

---
+++ certs.tf
@@ -0,0 +1,33 @@
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "tls_self_signed_cert" "cert" {
+  key_algorithm   = "RSA"
+  private_key_pem = tls_private_key.key.private_key_pem
+
+  subject {
+    common_name  = "N26.com"
+    organization = "N26 Bank"
+  }
+
+  validity_period_hours = 99999
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key      = tls_private_key.key.private_key_pem
+  certificate_body = tls_self_signed_cert.cert.cert_pem
+}
+
+
+resource "aws_lb_listener_certificate" "example" {
+  listener_arn    = aws_lb_listener.https.arn
+  certificate_arn = aws_acm_certificate.cert.arn
+}
