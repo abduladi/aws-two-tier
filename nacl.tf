a2706f1fd40c8df01113847e837bb5f9901af81b
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-20 13:09:25 +0200

    re-enabled private nacl

--- nacl.tf
+++ nacl.tf
@@ -42,60 +42,55 @@
 
 # # NACL for App server subnets
 
-# resource "aws_network_acl" "app_nacl" {
-#   vpc_id = aws_vpc.vpc.id
+resource "aws_network_acl" "app_nacl" {
+  vpc_id = aws_vpc.vpc.id
 
-#   subnet_ids = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+  subnet_ids = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
 
-#   ingress {
-#     rule_no    = 100
-#     protocol   = "tcp"
-#     action     = "allow"
-#     cidr_block = var.public-subnet-1-cidr
-#     from_port  = 443
-#     to_port    = 443
-#   }
-#   ingress {
-#     rule_no    = 101
-#     protocol   = "tcp"
-#     action     = "allow"
-#     cidr_block = var.public-subnet-2-cidr
-#     from_port  = 443
-#     to_port    = 443
-#   }
+# Allow inbound traffic from the VPC CIDR on the ALB listener port and health check port (port 80 serves both purposes)
+  ingress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc-cidr
+    from_port  = 443
+    to_port    = 443
+  }
 
-#   ingress {
-#     rule_no    = 102
-#     protocol   = "tcp"
-#     action     = "allow"
-#     cidr_block = var.public-subnet-1-cidr
-#     from_port  = 80
-#     to_port    = 80
-#   }
-#   ingress {
-#     rule_no    = 103
-#     protocol   = "tcp"
-#     action     = "allow"
-#     cidr_block = var.public-subnet-2-cidr
-#     from_port  = 80
-#     to_port    = 80
-#   }
+  ingress {
+    rule_no    = 101
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc-cidr
+    from_port  = 80
+    to_port    = 80
+  }
+
+
+  ingress {
+    rule_no    = 101
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc-cidr
+    from_port  = 80
+    to_port    = 80
+  }
   
-#   # deny all outbound traffic
-#   egress {
-#     rule_no    = 100
-#     protocol   = "-1"
-#     action     = "allow"
-#     cidr_block = "0.0.0.0/0"
-#     from_port  = 0
-#     to_port    = 0
-#   }
+  # Allow outbound traffic to the VPC CIDR on the ephemeral ports
+  egress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
 
 
-#   tags = {
-#     Name = "App Subnet NACL"
-#   }
-# }
+  tags = {
+    Name = "App Subnet NACL"
+  }
+}
 
 
 # NACL for public subnet
@@ -149,7 +144,7 @@ resource "aws_network_acl" "public_nacl" {
     to_port    = 80
   }
 
-  # allow all outbound traffic
+  # Allow outbound traffic on the ephemeral ports
   egress {
     rule_no    = 102
     protocol   = "tcp"
