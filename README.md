28fb3f83c3981115b882098d762f61445692bf92
Abdulwahab Adi <abduladi@users.noreply.github.com>
2024-06-20 12:53:47 +0200

    re-enabled nacl

--- README.md
+++ README.md
@@ -8,13 +8,17 @@ Security Capabilities
 
 installed ssl in certificate maanager and attached to application load balancer's listener.
 
-enforced redirect of HTTP traffic to HTTPS
+enforced redirect of HTTP traffic to HTTPS. These two are to support the encryption of data in transit
 
 Implemeted web application firewall on the application load balancer
 
 configured autoscaling policy to automate resiliency
 
 implemented secruity groups to control traffic for each subnet. This ensures only required traffic needed for smooth running of the architecture are permitted.
+
+Implemented data encryption for all data at rest both in database and s3 buckets.
+
+Ensured data stores (DB and s3 are private and not publicly accessible)
 
 ensured high availability by distributing each network subnet tier of the architecture into multiple availablity zones
 
--- nacl.tf
+++ nacl.tf
@@ -98,42 +98,69 @@
 # }
 
 
-# # NACL for public subnet
-# resource "aws_network_acl" "public_nacl" {
-#   vpc_id = aws_vpc.vpc.id
+# NACL for public subnet
+resource "aws_network_acl" "public_nacl" {
+  vpc_id = aws_vpc.vpc.id
 
-#   subnet_ids = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  subnet_ids = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 
-#   ingress {
-#     rule_no    = 100
-#     protocol   = "tcp"
-#     action     = "allow"
-#     cidr_block = "0.0.0.0/0"
-#     from_port  = 443
-#     to_port    = 443
-#   }
+  ingress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
 
-#   ingress {
-#     rule_no    = 101
-#     protocol   = "tcp"
-#     action     = "allow"
-#     cidr_block = "0.0.0.0/0"
-#     from_port  = 80
-#     to_port    = 80
-#   }
+  ingress {
+    rule_no    = 101
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
 
-#   # deny all outbound traffic
-#   egress {
-#     rule_no    = 100
-#     protocol   = "-1"
-#     action     = "allow"
-#     cidr_block = "0.0.0.0/0"
-#     from_port  = 0
-#     to_port    = 0
-#   }
+  ingress {
+    rule_no    = 102
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc-cidr
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  egress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc-cidr
+    from_port  = 443
+    to_port    = 443
+  }
+
+  egress {
+    rule_no    = 101
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc-cidr
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # allow all outbound traffic
+  egress {
+    rule_no    = 102
+    protocol   = "-1"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
 
 
-#   tags = {
-#     Name = "Public Subnet NACL"
-#   }
-# }
+  tags = {
+    Name = "Public Subnet NACL"
+  }
+}
